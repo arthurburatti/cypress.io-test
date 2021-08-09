67f05e8b873b5a1f4389fd4a7aea82bfcc6b46f9
Arthur de Figueredo Buratti <arthur.buratti@depssc.local>
2021-08-09 18:02:26 -0300

    Aula 1

---
+++ cypress/integration/describe.spec.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress" />
+
+it('Teste externo', () => {
+
+})
+
+describe('Grupo de teste', () => {
+    describe('Grupo de teste especifico', () => {
+        
+    })
+    it('Teste externo', () => {
+
+    })  
+})
