ca4fbc873941642eb308f7f90a08a9ee6bb6649d
Arthur de Figueredo Buratti <arthur.buratti@deps.com.br>
2021-08-10 18:06:35 -0300

    Aula 4

---
+++ cypress/integration/sync.spec.js
@@ -0,0 +1,29 @@
+/// <reference types="cypress" />
+
+describe('Espera', () => {
+    before(() => {
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+    })
+
+    beforeEach(() => {
+        cy.reload()
+    })
+
+
+    it('aguardar elemento ficar disponivel', () => {
+        cy.get('#novoCampo').should('not.exist')
+        cy.get('#buttonDelay').click()
+        cy.get('#novoCampo').should('not.exist')
+        cy.get('#novoCampo').should('exist')
+        cy.get('#novoCampo').type('funciona')
+    })
+
+    it.only('fazer retrys', () => {
+        cy.get('#buttonDelay').click()
+        cy.get('#novoCampo')
+            //.should('not.exist')
+            .should('exist')
+            .type('funciona')
+    })
+
+})
