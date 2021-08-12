144b2047e0ab41f42857c85e868499956ab30f47
Arthur de Figueredo Buratti <arthurfburatti@gmail.com>
2021-08-12 18:00:54 -0300

    Aula 7

---
+++ cypress/integration/alert.spec.js
@@ -0,0 +1,27 @@
+/// <reference types="cypress" />
+
+describe('alert', () => {
+    before(() => {
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+    })
+
+    beforeEach(() => {
+        cy.reload()
+    })
+
+    it('alert', () => {
+        cy.get('#alert').click()
+        cy.on('window:alert', msg => {
+            console.log(msg)
+            expect(msg).to.be.equal('Alert Simples')
+        })
+    })
+
+    it.only('alert com mock', () => {
+        const stub = cy.stub().as('alerta')
+        cy.on('window:alert', stub)
+        cy.get('#alert').click().then(() => {
+            expect(stub.getCall(0)).to.be.calledWith('Alert Simples')
+            })
+        })
+})
--- cypress/integration/elementos.spec.js
+++ cypress/integration/elementos.spec.js
@@ -77,9 +77,32 @@ describe('elementos basicos', () => {
         cy.get('[data-test=dataEscolaridade]')
             .select('1graucomp')
             .should('have.value','1graucomp' )
+            
+            
+        cy.get('[data-test=dataEscolaridade] option')
+            .should('have.length', 8)
+        
+        cy.get('[data-test=dataEscolaridade] option')
+            .then($arr => {
+                const values = []
+                $arr.each(function() {
+                    values.push(this.innerHTML)
+                })
+                expect(values).to.include.members(["Superior", "Mestrado"])
+            })
     })
 
+
     it.only('Combo multiplo', () => {
-        cy.get('[data-testid=dataEsportes]').select(['natacao', 'Corrida', 'nada'])
+        cy.get('[data-testid=dataEsportes]')
+            .select(['natacao', 'Corrida', 'nada'])
+        cy.get('[data-testid=dataEsportes]')
+            .then($el => {
+                expect($el.val()).to.be.deep.equal(['natacao', 'Corrida', 'nada'])
+                expect($el.val()).to.have.length(3)
+            })
+        cy.get('[data-testid=dataEsportes]')
+            .invoke('val')
+            .should('eql', ['natacao', 'Corrida', 'nada'])
     })
 })
