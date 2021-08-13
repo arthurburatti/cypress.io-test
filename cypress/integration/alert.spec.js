8226322217d9dd3e9b44f52826429fda70da02d0
Arthur de Figueredo Buratti <arthurfburatti@gmail.com>
2021-08-13 13:30:59 -0300

    Aula 8

--- cypress/integration/alert.spec.js
+++ cypress/integration/alert.spec.js
@@ -24,4 +24,38 @@ describe('alert', () => {
             expect(stub.getCall(0)).to.be.calledWith('Alert Simples')
             })
         })
+
+    it.only('Comfirma', () => {
+        cy.on('window:confirm', msg => {
+            expect(msg).to.be.equal('Confirm Simples')
+        })
+        cy.on('window:alert', msg => {
+            expect(msg).to.be.equal('Confirmado')
+        })
+        cy.get('#confirm').click()
+    })
+
+    it.only('Deny', () => {
+        cy.on('window:confirm', msg => {
+            expect(msg).to.be.equal('Confirm Simples')
+            return false
+        })
+        cy.on('window:alert', msg => {
+            expect(msg).to.be.equal('Negado')
+        })
+        cy.get('#confirm').click()
+    })
+
+    it.only('prompt', () => {
+        cy.window().then(win => {
+            cy.stub(win, 'prompt').returns('42')
+        })
+        cy.on('window:confirm', msg => {
+            expect(msg).to.be.equal('Era 42?')
+        })
+         cy.on('window:alert', msg => {
+            expect(msg).to.be.equal(':D')
+        })
+        cy.get('#prompt').click()
+    })
 })
---
+++ cypress/integration/desafio.spec.js
@@ -0,0 +1,32 @@
+/// <reference types="cypress" />
+
+describe('alert', () => {
+    before(() => {
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+    })
+
+    beforeEach(() => {
+        cy.reload()
+    })
+
+    it('Desafio', () => {
+        const stub = cy.stub().as('alerta')
+        cy.on('window:alert', stub)
+        cy.get('#formCadastrar').click()
+            .then(() => expect(stub.getCall(0)).to.be.calledWith('Nome eh obrigatorio'))
+        
+        cy.get('#formNome').type('Teste1')
+        
+        cy.get('#formCadastrar').click()
+            .then(() => expect(stub.getCall(1)).to.be.calledWith('Sobrenome eh obrigatorio'))
+
+        cy.get('[data-cy=dataSobrenome]').type('Teste2')
+        cy.get('#formCadastrar').click()
+            .then(() => expect(stub.getCall(2)).to.be.calledWith('Sexo eh obrigatorio'))
+
+        cy.get('#formSexoMasc').click()
+        cy.get('#formCadastrar').click()
+        
+        cy.get('#resultado > :nth-child(1)').should('contain', 'Cadastrado')
+    })
+})
