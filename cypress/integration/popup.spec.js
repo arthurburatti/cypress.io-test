b1ee57c1c7bdb01712e460ff6a072316d95af6d1
Arthur de Figueredo Buratti <arthurfburatti@gmail.com>
2021-08-17 12:58:23 -0300

    Aula 10

---
+++ cypress/integration/popup.spec.js
@@ -0,0 +1,48 @@
+/// <reference types="cypress" />
+
+describe('Popup', () => {
+    it('Deve testar popup diretamente', () => {
+        cy.visit('https://www.wcaquino.me/cypress/frame.html')
+        cy.get('#otherButton').click()
+        cy.on('window:alert', msg => {
+            expect(msg).to.be.equal('Click OK!')
+        })
+        
+    })
+
+    it.only('Deve verificar se o popup foi invocado', () => {
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+        cy.window().then(win => {
+            cy.stub(win, 'open').as('winOpen')
+        })
+        cy.get('#buttonPopUp').click()
+        cy.get('@winOpen').should('be.called')
+    })
+
+})
+
+describe.only('Popup links', () => {
+    beforeEach(() => {
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+    })
+    it('checar popup link', () => {
+        cy.contains('Popup2')
+            .should('have.prop', 'href')
+            .and('equal', 'https://www.wcaquino.me/cypress/frame.html')
+    })
+    it('acessar popup dinamica', () => {
+        cy.contains('Popup2').then($a => {
+            const href = $a.prop('href')
+            cy.visit(href)
+            cy.get('#tfield').type('funciona')
+            
+        })
+    })
+
+    it('forçar o link abrir na mesma pagina', () => {
+        cy.contains('Popup2')
+            .invoke('removeAttr', 'target')
+            .click()
+        cy.get('#tfield').type('funciona')
+    })
+})
