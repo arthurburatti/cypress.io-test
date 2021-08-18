af14a321e7c67ab02c052934d988a2472c289a7f
Arthur de Figueredo Buratti <arthurfburatti@gmail.com>
2021-08-18 16:09:38 -0300

    Aula 11

---
+++ cypress/integration/locators.spec.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+describe('elementos basicos', () => {
+    before(() => {
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+    })
+
+    beforeEach(() => {
+        cy.reload()
+    })
+
+    it('...', () => {
+       
+    })
+})
