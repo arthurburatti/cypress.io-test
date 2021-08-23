82da5bb0d8fa6412f200c8713f7c94020f8a59c9
Arthur de Figueredo Buratti <arthurfburatti@gmail.com>
2021-08-23 14:00:47 -0300

    Aula 14

---
+++ cypress/integration/time.spec.js
@@ -0,0 +1,47 @@
+/// <reference types="cypress" />
+
+describe('elementos basicos', () => {
+    before(() => {
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+    })
+
+    beforeEach(() => {
+        cy.reload()
+    })
+
+
+    it('Voltar', () => {
+        cy.get('#buttonNow').click()
+        cy.get('#resultado').should('contain', '23/08/2021')
+
+/*         cy.clock()
+        cy.get('#buttonNow').click()
+        cy.get('#resultado').should('contain', '23/08/2021') */
+
+        const dt = new Date(2012, 3, 10, 15, 23, 50)
+        cy.clock(dt.getTime())
+        cy.get('#buttonNow').click()
+        cy.get('#resultado').should('contain', '10/04/2012')
+    })
+
+    it.only('Futuro', () => {
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').should('contain', '16297')
+        cy.get('#resultado > span').invoke('text').should('gt', 1334082230000)
+
+        cy.clock()
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').invoke('text').should('lte', 0)
+        cy.wait(1000)
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').invoke('text').should('lte', 1000)
+
+        cy.tick(5000)
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').invoke('text').should('gte', 5000)
+
+        cy.tick(10000)
+        cy.get('#buttonTimePassed').click()
+        cy.get('#resultado > span').invoke('text').should('gte', 15000)
+    })
+})
